7887072961846ff001b64fa51e8497f3eda3254d
adachitakuya <taku.hiroguchi2@gmail.com>
2023-02-07 05:37:43 +0900

    button chenge

--- app/src/main/java/com/example/repcalendar/pinkmouse.kt
+++ app/src/main/java/com/example/repcalendar/pinkmouse.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MouseFragment.newInstance] factory method to
+ * Use the [pinkmouse.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MouseFragment : Fragment() {
+class pinkmouse : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class MouseFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mouse, container, false)
+        return inflater.inflate(R.layout.fragment_pinkmouse, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class MouseFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MouseFragment.
+         * @return A new instance of fragment pinkmouse.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MouseFragment().apply {
+            pinkmouse().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
