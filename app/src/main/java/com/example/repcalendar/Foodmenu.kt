0c4c89e7f4a6e53f29806c5787417c62268e9cd5
adachitakuya <taku.hiroguchi2@gmail.com>
2023-02-13 17:52:21 +0900

    tapped

--- app/src/main/java/com/example/repcalendar/Foodmenu.kt
+++ app/src/main/java/com/example/repcalendar/Foodmenu.kt
@@ -3,31 +3,27 @@ package com.example.repcalendar
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.repcalendar.databinding.ActivityFoodmenuBinding
-import com.example.repcalendar.databinding.ActivityMainBinding
 
 class Foodmenu : AppCompatActivity() {
     private lateinit var binding: ActivityFoodmenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_foodmenu)
+        binding = ActivityFoodmenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
 
-        fun onFoodButtonTapped(ciew: View?) {
+        fun onFoodButtonTapped(view: View?) {
             val intent = Intent(this, size::class.java)
             startActivity(intent)
+
         }
         binding.clicketButton.setOnClickListener {onFoodButtonTapped(it)}
         binding.cockloachButton.setOnClickListener{onFoodButtonTapped(it)}
         binding.mouseButton.setOnClickListener{onFoodButtonTapped(it)}
         binding.pinkmouseButton.setOnClickListener{onFoodButtonTapped(it)}
-        binding.gelButton.setOnClickListener{onFoodButtonTapped(it)}
-        binding.vegiButton.setOnClickListener{onFoodButtonTapped(it)}
-        binding.wormButton.setOnClickListener{onFoodButtonTapped(it)}
 
 
     }
