51266dbe92edf7fda7868cac9b47d6df1c5980e0
adachitakuya <taku.hiroguchi2@gmail.com>
2023-05-10 19:25:29 +0900

    5/10

--- app/src/main/java/com/example/repcalendar/MainActivity.kt
+++ app/src/main/java/com/example/repcalendar/MainActivity.kt
@@ -9,6 +9,8 @@ import com.example.repcalendar.databinding.ActivityMainBinding
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.query.RealmResults
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.query.Sort
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,23 +21,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // items in the realm whose name begins with the letter 'D'
-        val itemsThatBeginWIthD: RealmResults<Realmfile> =
-            realm.query<Realmfile>("name")
-                .find()
-
-        val size: RealmResults<Realmfile> =
-            realm.query<Realmfile>("size")
-                .find()
-
-        val number: RealmResults<Realmfile> =
-            realm.query<Realmfile>("number")
-                .find()
-
 
         // all items in the realm
-        //val name: RealmResults<Realmfile> = realm.query<Realmfile>("name").find()
-        //val size: RealmResults<Realmfile> = realm.query<Realmfile>("size").find()
+        val name: RealmResults<Realmfile> = realm.query<Realmfile>.sort("date", Sort.DESCENDING).find()
 
         realm.close()
 
